a1e01a1f79703721e78c5628e3989ee7b0f327d1
siberia.hu <WiseChatTeahouse@gmail.com>
2024-06-14 18:30:32 +0800

    feat:基础配置类

---
+++ src/main/java/chat/wise/message/wecom/WecomAutoConfiguration.java
@@ -0,0 +1,14 @@
+package chat.wise.message.wecom;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+/**
+ * @Author siberia.hu
+ * @Package chat.wise.message.wecom
+ * @Date 2024/6/14 18:29
+ */
+@AutoConfiguration
+@EnableConfigurationProperties(WecomProperties.class)
+public class WecomAutoConfiguration {
+}
---
+++ src/main/java/chat/wise/message/wecom/WecomProperties.java
@@ -0,0 +1,12 @@
+package chat.wise.message.wecom;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @Author siberia.hu
+ * @Package chat.wise.message.wecom
+ * @Date 2024/6/14 18:28
+ */
+@ConfigurationProperties(prefix = "wise.chat.message.wecom")
+public class WecomProperties {
+}
